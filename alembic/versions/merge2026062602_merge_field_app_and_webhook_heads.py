02de6cc66a1b138a6ab1f20ad14e3f7e65e32ad3
root <root@vmi3027474.contaboserver.net>
2026-06-25 16:43:52 +0200

    Merge webhook migration head

---
+++ alembic/versions/merge2026062602_merge_field_app_and_webhook_heads.py
@@ -0,0 +1,20 @@
+"""Merge field app and webhook migration heads.
+
+Revision ID: merge2026062602
+Revises: uc2026062600, wh2026062600
+Create Date: 2026-06-26 00:02:00.000000
+"""
+
+# revision identifiers, used by Alembic.
+revision = "merge2026062602"
+down_revision = ("uc2026062600", "wh2026062600")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
