ca114d671e24622b250ef0b84e91b4d5a25dea06
root <root@vmi3027474.contaboserver.net>
2026-06-28 23:06:54 +0200

    Merge field asset migration heads

--- alembic/versions/adad6ae61bb0_merge_field_asset_hardening_into_main.py
+++ alembic/versions/adad6ae61bb0_merge_field_asset_hardening_into_main.py
@@ -1,17 +1,16 @@
 """merge field asset hardening into main
 
 Revision ID: adad6ae61bb0
-Revises: ih2026062800, qf7a8b9c0d1e
+Revises: qf7a8b9c0d1e, ld2026062900, rs2026062801, sh2026062900
 Create Date: 2026-06-28 20:27:53.060738
 
 """
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 
-
-revision = 'adad6ae61bb0'
-down_revision = ('ih2026062800', 'qf7a8b9c0d1e')
+revision = "adad6ae61bb0"
+down_revision = ("qf7a8b9c0d1e", "ld2026062900", "rs2026062801", "sh2026062900")
 branch_labels = None
 depends_on = None
 
